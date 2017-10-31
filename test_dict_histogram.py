167c04a14f2f6af8d3afa592f94301daf49a0daa
juan hurtado <juanhurtado4@outlook.com>
2017-10-31 14:30:28 -0700

    Created test for get_histogram function

--- test_dict_histogram.py
+++ test_dict_histogram.py
@@ -19,4 +19,7 @@ def test_get_clean_data():
     assert '!' not in get_clean_data(raw_data)
 
 def test_get_histogram():
-    assert
+    clean_data = get_clean_data(raw_data)
+    histogram = get_histogram(clean_data)
+    values_type = [True for num in histogram.values() if type(num) == int]
+    assert False not in values_type ## Checks if all values are integers
