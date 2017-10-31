04468e05385790b42be47491805547ed48baec75
juan hurtado <juanhurtado4@outlook.com>
2017-10-31 14:04:03 -0700

    Finished get_histogram function

--- histogram.py
+++ histogram.py
@@ -6,15 +6,22 @@ import string
 # TODO: Finish unique_words function
 # TODO: Finish frequency function
 
-def histogram(source_text):
+def get_histogram(source_text):
     '''
     Source_text: String
     Function analyses the frequency of words in Source_text
     Returns key value pair. Key: String, Value: Int
     '''
-    pass
+    histogram = {}
 
-def unique_words(histogram):
+    for word in source_text:
+        if word not in histogram:
+            histogram[word] = 1
+            continue
+        histogram[word] += 1
+    return histogram
+
+def get_unique_words(histogram):
     '''
     Histogram: Key value pair. Key: String, Value: Int
     Function counts the total unique words in the histogram
@@ -22,7 +29,7 @@ def unique_words(histogram):
     '''
     pass
 
-def frequency(word, histogram):
+def get_frequency(word, histogram):
     '''
     Word: String
     Histogram: Key value pair. Key: String, Value: Int
@@ -31,20 +38,27 @@ def frequency(word, histogram):
     '''
     pass
 
-def clean_data(raw_data):
+def get_clean_data(raw_data):
 
-    punctuationless_data = ''.join([char for char in raw_data
+    nonspoken_content_removed = re.sub('\(\w*\)', '', raw_data)
+
+    nonspoken_content_removed = re.sub('\(\w*\s\w*\)', '', nonspoken_content_removed)
+
+    numbers_removed = re.sub('\d\w*', '', nonspoken_content_removed)
+
+    punctuationless_data = ''.join([char for char in numbers_removed
                                     if char not in string.punctuation])# Removes punctuation from data
 
     clean_data = re.split('\s*', punctuationless_data)[:-1]  # Splits data based on whitespace
-    pass
+
+    return clean_data
 
 def main():
     with open('source_text.txt') as file:
 
-        raw_data = file.read()
+        raw_data = file.read().lower()
 
-        clean_data(raw_data)
+        clean_data = get_clean_data(raw_data)
 
 if __name__=='__main__':
     main()
