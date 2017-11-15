3ec3e29f88995f4ff0e5a7425922b65c3f2b32cc
juan hurtado <juanhurtado4@outlook.com>
2017-11-14 22:28:55 -0800

    Create test function for sentence generator

--- markov_sentence_generator.py
+++ markov_sentence_generator.py
@@ -28,10 +28,6 @@ def get_random_word(histogram):
     Histogram: Key Value pair. Key: String, Value: Int
     Returns a single word at random
     '''
-    # {
-        # 'fish': {'one': 1, 'red: 2'},
-        # 'red': {'fish': 1, 'one': 2}
-    # }
 
     rand_num = random.random()
 
@@ -39,9 +35,9 @@ def get_random_word(histogram):
 
     for key, value in histogram.items():
 
-        word_likelyhood_percent = value / sum(histogram.values())
+        word_probability = value / sum(histogram.values())
 
-        cummulitive_wght += word_likelyhood_percent
+        cummulitive_wght += word_probability
 
         if rand_num <= cummulitive_wght:
             random_word = key
@@ -49,40 +45,13 @@ def get_random_word(histogram):
 
 
 
-    # print('----------random number:', rand_num)
-    # print('\n')
-    # print('----------Final result------------')
-    # print('\n')
-    # print('----------cummulitive_wght:', cummulitive_wght)
-    # print('\n')
-    # print('----------random number:', rand_num)
-    # print('\n')
-    # print('----------random word:', random_word)
-    # print()
     return random_word
-'''
-One solution to iterate through nested dictionaries
-stop = ''
-for outer_key, outer_value in histogram.items():
-    if stop == True:
-        break
-    for inner_key, inner_value in outer_value.items():
-        word_likelyhood_percent = inner_value / sum(outer_value.values())
-
-        cummulitive_wght += word_likelyhood_percent
-
-        print('----------cummulitive_wght:', cummulitive_wght)
-        if rand_num <= cummulitive_wght:
-            random_word = inner_key
-            stop = True
-            break
-        '''
 
 def sentence_generator(num_words_in_sentence, histogram):
     '''
     Num_words_in_sentence: Int
     Histogram: Key Value pair. Key: String | Value: Int
-    Function generates a sentence from a source text
+    Function generates a sentence from a markov_chain
     Returns a string
     '''
     sentence = ''
@@ -118,77 +87,39 @@ def get_histogram(word_list):
             result[word] = {next_word: 1}
 
         else:
-            # {
-                # 'fish': {'one': 1, 'red: 2'},
-                # 'red': {'fish': 1, 'one': 2}
-            # }
 
             if next_word not in result[word]:
+
                 result[word].update({next_word: 1})
+
             else:
                  result[word][next_word] += 1
     return result
 
+def test_frequency():
+    '''
+    Function test sentence generator
+    '''
+    from dictionary_histogram import get_histogram as test
+
+    markov_chain = {'one': {'fish': 1}, 'fish': {'two': 1, 'red': 1, 'blue': 2}, 'two': {'fish': 1}, 'red': {'fish': 1}, 'blue': {'fish': 1}}
+
+    # list_of_words = get_clean_data(sentence_generator(1000, markov_chain))
+    list_of_words = sentence_generator(1000, markov_chain).lower().split(' ')
+
+    histogram = test(list_of_words)
+
+    return histogram
+
 if __name__=='__main__':
-    with open('obama_speech.txt') as file:
+    # with open('obama_speech.txt') as file:
+    with open('one_fish_text.txt') as file:
 
         raw_data = file.read().lower()
 
     clean_data = get_clean_data(raw_data)
     histogram = get_histogram(clean_data)
-    print(sentence_generator(10, histogram))
+    print(histogram)
+    # print(sentence_generator(10, histogram))
 
-    # histogram = {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1}
-    # histogram = {'one': {'fish': 1}, 'fish': {'two': 1, 'red': 1, 'blue': 1}, 'two': {'fish': 1}, 'red': {'fish': 1}, 'blue': {'fish': 1}}
-
-'''
-# -----------------------------------------
-
-# Final format of how we want our histogram to be modeled
-# Follow this format
-{
-    'one': {'fish': 1},
-    'fish': {'blue': 1, 'red': 1, 'two': 1},
-    'blue': {'fish': 1},
-    'two': {'fish': 1},
-    'red': {'fish': 1}
-}
-
-
-# List of words
-['one', 'fish', 'blue', 'fish', 'red', 'fish', 'two', 'fish']
-
-# Step 1) Get a word
-# Step 2) Get the word that comes after
-# Step 3) Store word as a key in dictionary
-# Step 4) Store the word that comes after as a VALUE
-# Step 5) If word that comes after already in nested dictionary increment by 1
-
-def get_histogram(list_of_words):
-    #Converts a list of words into a markov chain
-
-    histogram = {}
-    # Step 1
-    for index, word in enumerate(list_of_words): # enumerate allows you to iterate through index AND value
-        # index: 0
-        # word: one
-
-        # Step 2
-        word_that_comes_after = list_of_words[index + 1] # fish
-
-        # step 3
-        if word_that_comes_after not in histogram[word]:
-
-            # step 4
-            histogram[word] = {word_that_comes_after: 1}
-
-        else:
-            # step 5
-            histogram[word][word_that_comes_after] += 1
-
-histogram[word] >> {'fish': 1}
-
-histogram[word][word_that_comes_after] >> 1
-
-'one': {'fish': 1}
-'''
+    # print(test_frequency())
