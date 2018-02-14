5f2d8818fd155d547388b9ea2a6046f009481692
juan hurtado <juanhurtado4@outlook.com>
2018-02-13 22:38:41 -0800

    Feat: Implement third order markov chain

--- markov_sentence_generator.py
+++ markov_sentence_generator.py
@@ -102,12 +102,33 @@ def get_histogram(word_list):
     # def get_histogram(word_list):
     
     # Second order markov chain implemented below
+    # result = {}
+    # for index, word in enumerate(word_list):
+    #     try:
+    #         next_word = word_list[index + 1]
+    #         after_next = word_list[index + 2]
+    #         next_state = next_word + ' ' + after_next
+    #     except:
+    #         break
+
+    #     if word not in result:
+
+    #         result[word] = {next_state: 1}  # Implement as tuple or string
+
+    #     else:
+
+    #         if next_state not in result[word]:
+
+    #             result[word].update({next_state: 1})
+
+    #         else:
+    #              result[word][next_state] += 1
+    # return result
     result = {}
+    order = 3
     for index, word in enumerate(word_list):
         try:
-            next_word = word_list[index + 1]
-            after_next = word_list[index + 2]
-            next_state = next_word + ' ' + after_next
+            next_state = ' '.join(word_list[index + 1: index + order])
         except:
             break
 
